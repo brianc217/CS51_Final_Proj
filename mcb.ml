8f7311828f8b923b51c56143ff5156b1ca6302c0
hmaclean <hkgmaclean@gmail.com>
2011-04-10 20:28:37 -0700

    Edited mcb.ml via GitHub

--- mcb.ml
+++ mcb.ml
@@ -13,3 +13,49 @@ sig
   val make_dict: token list -> dict
   val babble: key -> dict -> string
 end
+
+module MCB_imp : MCB
+struct
+  type token = string;;
+  type key = (token*token);;
+  type value = (token*float) list;;
+  type dict = (key*value) list;;
+  let empty = [];;
+  let rec insert (d:dict) (k:key) (v:value) : dict = 
+    match d with
+      | [] -> [(k,v)]
+      | _ -> 
+end
+
+module 
+struct
+  module M = Map.make(
+    struct
+      type t =
+    end)
+  type token = string
+  type key = (string*string)
+  type value = (token*token)
+  type dict = 
+  let empty = M.empty
+  let insert (d:dict) (k:key) (v:value) : dict = M.add k v d
+  let lookup (d:dict) (k:key) : value option =
+    try
+      Some (M.find k d)
+    with Not_found ->
+      None
+  let member (d:dict) (k:key) : bool = 
+    M.mem k d
+  let read (file: 'a) : token list =
+
+
+  let normalize (d:dict) : dict =
+
+
+  let make_dict (t:token list) : dict =
+
+
+  let babble (k:key) (d:dict) : string =
+
+
+end
