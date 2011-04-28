36592f229c60ad4dd4324374039f72ddcb80b64f
brianc217 <brianc217@gmail.com>
2011-04-28 14:32:35 -0400

    Combining

--- combine.ml
+++ combine.ml
@@ -1,4 +1,11 @@
 open Grammar;;
 open MCB;;
 
-let pslist = randomsentence();;
+let poslist = randomsentence();;
+
+let babble (list: string list) (markov: dict) (word: dict): string =
+  let rec helper list markov word string : string =
+    match list with
+      | [] -> string
+      | hd::tl -> 
+
